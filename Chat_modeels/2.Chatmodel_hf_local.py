abd8ed299ceaf43bf7cee5549cb6f182d2c7bb7e
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-04 15:31:16 +0530

    Run Model Locally

---
+++ Chat_modeels/2.Chatmodel_hf_local.py
@@ -0,0 +1,14 @@
+from langchain_huggingface import HuggingFacePipeline, ChatHuggingFace
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='MiniMaxAI/MiniMax-M2.1',
+    task='text-generation',
+    device='cuda',
+    pipeline_kwargs=dict(
+        temprature=0.5,
+        max_new_tokens=100
+    ))
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke('what is currency of india')
+print(result.content)
