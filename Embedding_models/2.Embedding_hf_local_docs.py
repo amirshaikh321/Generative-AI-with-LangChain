65b53196397aadc832f1633c9f0bbaf77544e128
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-09 23:32:24 +0530

    embedding docs

---
+++ Embedding_models/2.Embedding_hf_local_docs.py
@@ -0,0 +1,11 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L12-v2')
+
+docs= ["Delhi is the captital of India",
+       "kolkata is the capital of west bengal",
+       "paris is the capital of France"]
+
+vector = embedding.embed_query(docs)
+
+print(str(vector))
