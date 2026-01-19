70cafbe7c6a26cba6082d147118816c62bc3254b
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-19 23:51:35 +0530

    model changed

--- googleai.py
+++ googleai.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv() 
 
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+llm = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
 
-response = llm.invoke("Hello, Gemini!")
+response = llm.invoke("who is pm of india")
 print(response.content)
