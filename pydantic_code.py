089e20ab7a8584f0a15aad293d7bac2d9ded72c5
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-21 23:13:02 +0530

    convert to dictionary

---
+++ pydantic_code.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+class Student(BaseModel):
+
+    name: str
+    age: Optional[int]
+    cgpa: float = Field(gt=0,lt=10)
+
+new_student = {'name':'amir','age':21, 'cgpa':'7.25'}
+
+student = Student(**new_student)
+student_dict = dict(student)
+print(student_dict['age'])
+student_json = student_dict.json()
