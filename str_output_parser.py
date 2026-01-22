7dd8c76a7d0028e5f50bd4e45ec57894f7b7e876
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-22 23:08:38 +0530

    string output parser

---
+++ str_output_parser.py
@@ -0,0 +1,25 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V3.2",
+                          task='text-generation')
+model = ChatHuggingFace(llm= llm)
+
+template1 = PromptTemplate(
+    template="write a detailed report on {topic}",
+    input_variables=['topic']
+)
+template2 = PromptTemplate(
+    template='write a 5 line summary on the following text. /n {text}',
+    input_variables=['text']
+)
+
+prompt1 = template1.invoke({'topic':'black hole'})
+result = model.invoke(prompt1)
+
+prompt2 = template2.invoke({'text':result.content})
+result =model.invoke(prompt2)
+print(result.content)
