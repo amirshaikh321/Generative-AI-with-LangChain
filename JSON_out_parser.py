0aa570ad4cdd95d9b9f0cdcf0b66f35f888bfca0
Amir Shaikh <amirshaikh1594@gmail.com>
2026-01-23 12:51:38 +0530

    JSON output parser

---
+++ JSON_out_parser.py
@@ -0,0 +1,19 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V3.2",
+                          task='text-generation')
+model = ChatHuggingFace(llm= llm)
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(template='Give me 5 facts about {topic}\n {format_instruction}',
+                          input_variables=['topic'],
+                          partial_variables={'format_instruction':parser.get_format_instructions})
+prompt = template.invoke({'topic':'black hole'})
+result = model.invoke(prompt)
+print(result.content)
