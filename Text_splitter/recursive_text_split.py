59883ad77e2a5e3efc51dc2ec3b6b515d32d012c
Amir Shaikh <amirshaikh1594@gmail.com>
2026-02-04 19:04:34 +0530

    Text splitter -> RecursiveCharacterTextSplitter

---
+++ Text_splitter/recursive_text_split.py
@@ -0,0 +1,13 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text = """Artificial intelligence is changing how people work and learn.
+Small ideas grow into powerful systems when data, logic, and creativity come together.
+Sometimes progress is fast, sometimes it pauses, but innovation never really stops.
+Technology keeps rewriting the rules of what is possible.
+"""
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 100,
+                                               chunk_overlap = 5)
+
+result = text_splitter.split_text(text=text)
+
+print(result[1])
